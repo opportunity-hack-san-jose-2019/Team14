a6d6103028df85bb167a76bdbd12e32d13f9bb11
Daniil Durnev <dandurnev1@gmail.com>
2019-11-02 15:30:08 -0700

    handling sign up button

--- Team14Project/src/app/app.component.ts
+++ Team14Project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,34 @@ export class AppComponent {
   firstName = '';
   lastName = '';
   email = '';
+  phone = '';
+  location = '';
   password = '';
 
 
   toggleRegistration() {
     this.isRegistering = !this.isRegistering;
+  }
+
+  signUp() {
+    var student = {
+      email: this.email,
+      password: this.password,
+      first: this.firstName,
+      last: this.lastName,
+      cohort: '',
+      location: this.location,
+      phone: this.phone,
+      interests: [],
+      attendance: 0,
+      project_score: 0,
+      bonus: 0,
+      total_score: 0,
+      event_list: []
+    }
+
+    axios.post('http://localhost:8080', student).then(response => console.log(response.data));
+
+    console.log(student);
   }
 }
