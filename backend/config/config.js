0a7b04ca75af0408057c4c926fa6ff75cffb9310
William Truong <lordvinh1@gmail.com>
2019-11-02 14:06:54 -0700

    Add backend configuration and install mongoose

---
+++ backend/config/config.js
@@ -0,0 +1,6 @@
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  process.env.PORT = 8080;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017';
+}
---
+++ backend/db/mongoose.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+module.exports = { mongoose };
