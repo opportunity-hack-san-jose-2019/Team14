34b16104111b0770235677b0a14e12a4bc367b80
Daniil Durnev <dandurnev1@gmail.com>
2019-11-02 16:10:20 -0700

    Authentication has been implemented

--- Team14Project/src/app/app-routing.module.ts
+++ Team14Project/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: WelcomePageComponent
+  },
+  {
+    path: 'profilePage', component: ProfilePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Team14Project/src/app/app.component.ts
+++ Team14Project/src/app/app.component.ts
@@ -7,42 +7,5 @@ import axios from 'axios';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appName = 'Braven';
-
-  isVolunteer = false;
-  isRegistering = true;
-
-  firstName = '';
-  lastName = '';
-  email = '';
-  phone = '';
-  location = '';
-  password = '';
-
-
-  toggleRegistration() {
-    this.isRegistering = !this.isRegistering;
-  }
-
-  signUp() {
-    var student = {
-      email: this.email,
-      password: this.password,
-      first: this.firstName,
-      last: this.lastName,
-      cohort: '',
-      location: this.location,
-      phone: this.phone,
-      interests: [],
-      attendance: 0,
-      project_score: 0,
-      bonus: 0,
-      total_score: 0,
-      event_list: []
-    }
-
-    axios.post('http://localhost:8080', student).then(response => console.log(response.data));
-
-    console.log(student);
-  }
+  
 }
