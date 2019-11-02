78973df6141202a4b5a77c89cdcac22ac4e47483
William Truong <lordvinh1@gmail.com>
2019-11-02 16:10:58 -0700

    fix uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://root:root@opportunityhackcluster-w3hia.mongodb.net/test?retryWrites=true&w=majority',  { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = { mongoose };
