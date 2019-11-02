01399589ff57093db584d4fd0236581ba77a8e4a
William Truong <lordvinh1@gmail.com>
2019-11-02 14:56:26 -0700

    Add routes

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,5 +2,5 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 8080;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017';
+  process.env.MONGODB_URI = 'mongodb+srv://root:root@opportunityhackcluster-w3hia.mongodb.net/test?retryWrites=true&w=majority';
 }
