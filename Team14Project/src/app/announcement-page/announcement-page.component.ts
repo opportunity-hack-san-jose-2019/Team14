2f1dfe05b266cbad242729b79b0deae672cf308e
William Truong <lordvinh1@gmail.com>
2019-11-03 01:01:39 -0800

    Event changes

--- Team14Project/src/app/announcement-page/announcement-page.component.ts
+++ Team14Project/src/app/announcement-page/announcement-page.component.ts
@@ -10,6 +10,7 @@ import {
   } from '@angular/material/table';
   import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import Axios from 'axios';
   
   @Component({
     selector: 'app-announcement-page',
@@ -21,15 +22,26 @@ import { UserService } from '../services/user.service';
   
     @ViewChild('eventTable', {static: false}) eventTable: MatTable < any > ;
     currentUser : Object;
+    eventList: []
 
     constructor(private router: Router, private userService: UserService) {
+      this.eventList = []
     }
   
     ngOnInit() {
       this.userService.getCurrentUser().then((user) => {
-          console.log(user)
-          this.currentUser = user
+        console.log(user)
+        this.currentUser = user
+      });
+
+      Axios.get('https://obscure-badlands-88487.herokuapp.com/event/upcoming')
+      .then((events) => {
+        console.log(events);
+        this.eventList = events.data;
+      }).catch((e) => {
+        console.log(e)
       })
+
     }
   
     logOut() {
